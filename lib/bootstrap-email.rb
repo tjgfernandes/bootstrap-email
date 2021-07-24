ced2737c7fc5a3b9eaca4ec665ed574b669cbd69
Tiago Fernandes <tjg.fernandes@gmail.com>
2021-07-24 20:43:32 +0100

    matching bootstrap col-

--- lib/bootstrap-email.rb
+++ lib/bootstrap-email.rb
@@ -147,7 +147,7 @@ module BootstrapEmail
       each_node('.row') do |node|
         node.replace(template('row', classes: node['class'], contents: node.inner_html))
       end
-      each_node('*[class*=col]') do |node|
+      each_node('*[class*=col-]') do |node|
         node.replace(template('col', classes: node['class'], contents: node.inner_html))
       end
     end
